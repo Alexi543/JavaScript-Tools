00d4c766d04d916d74df13db4be1320f1b821520
Alexi543 <alexilord2008@gmail.com>
2022-04-04 18:10:14 +0000

    14:10/04/04/22

--- debug/tmp.js
+++ debug/tmp.js
@@ -1,4 +1,3 @@
 import {random} from "../random.js";
 import {string} from "../string.js";
 
-for (let i = 0; i < 10; i++) console.log(random.float(1, 10));
--- debug/tmp.py
+++
@@ -1,5 +0,0 @@
-from itertools import count
-import string
-import random
-
-print(random.sample([1, 2, 3, 4, 5], 4))
