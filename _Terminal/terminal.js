3a838178704a9a468b59e3676efe37aff38e8e6c
UralimixYT <alexilord2008@gmail.com>
2022-04-26 02:18:34 -0400

    2:18/26/04/22

--- _Terminal/terminal.js
+++ _Terminal/terminal.js
@@ -1,44 +1,20 @@
 export const terminal = new class {
-    #root = document.querySelector(":root");
     #inputsNum = 0;
     #area;
 
     init(position = document.body) {
         document.head.innerHTML += "<link rel='stylesheet' href='/_Terminal/terminal.css'>";
-        position.innerHTML += "<section class='terminal'></section>";
+        position.innerHTML += (
+            "<section class='terminal'> \
+                <p class='terminal-title'>Terminal</p> \
+            </section>"
+        );
         this.#area = document.querySelector(".terminal");
-    }
-
-    backgound(color) {
-        if (typeof(color) !== "string" || color === "") return console.error("Set Background - Invalid Color");
-
-        this.#root.style.setProperty("--background", color);
-    }
-
-    color(color) {
-        if (typeof(color) !== "string" || color === "") return console.error("Set Color - Invalid Color");
-
-        this.#root.style.setProperty("--color", color);
-    }
-
-    size(size) {
-        if (typeof(size) !== "string" || size === "" || /x/.test(size) === false) return console.error("Set Size - Invalid Dimensions");
-        
-        size = size.split("x");
-
-        this.#root.style.setProperty("--terminal-width", `${size[0]}px`);
-        this.#root.style.setProperty("--terminal-heigth", `${size[1]}px`);
-    }
-
-    fontSize(size) {
-        if (typeof(size) !== "string" || size === "") return console.error("Set Size - Invalid Dimensions");
-
-        this.#root.style.setProperty("--font-size", size) 
     }
 
     print(...messages) {
         if (messages.length === 0) return console.error("Print - No Messages.");
-
+        
         messages.forEach(message => {
             if (message === "") return console.error("Print - Invalid Message");
             this.#area.innerHTML += `<pre class="prints">${message}</pre>`;
@@ -56,7 +32,7 @@ export const terminal = new class {
             this.#area.innerHTML += (
                 `<pre class="input-label">${message}</pre>
                 <input type='text' class="input" id="${this.#inputsNum}">`
-            );
+                );
         } else {
             this.#area.innerHTML += (
                 `<pre class="input-label">${message}</pre>
@@ -77,9 +53,72 @@ export const terminal = new class {
             func(entry);
         });
     }
+    
+    skipLine(skip = 1) {
+        for (let i = 0; i < skip; i++) this.#area.innerHTML += "<br>";
+    }
 
     clear() {
         this.#inputsNum = 0;
-        this.#area.innerHTML = "";
+        this.#area.innerHTML = (
+            "<div class='terminal-head'> \
+                <p id='terminal-title'>Terminal</p> \
+            </div> \
+            <div class='terminal'> \
+            \
+            </div>"
+        );
+    }
+
+    window = new class {
+        #root = document.querySelector(":root");
+
+        backgound(color) {
+            if (typeof(color) !== "string" || color === "") return console.error("Set Background - Invalid Color");
+    
+            this.#root.style.setProperty("--background", color);
+        }
+    
+        color(color) {
+            if (typeof(color) !== "string" || color === "") return console.error("Set Color - Invalid Color");
+    
+            this.#root.style.setProperty("--color", color);
+        }
+    
+        size(size) {
+            if (typeof(size) !== "string" || size === "" || /x/.test(size) === false) return console.error("Set Size - Invalid Dimensions");
+            
+            size = size.split("x");
+    
+            this.#root.style.setProperty("--terminal-width", `${size[0]}px`);
+            this.#root.style.setProperty("--terminal-heigth", `${size[1]}px`);
+        }
+    
+        position(pos) {
+            if (typeof(pos) !== "string" || pos === "" || /x/.test(pos) === false) return console.error("Set Position - Invalid Position");
+
+            pos = pos.split("x");
+
+            this.#root.style.setProperty("--pos-x", `${pos[0]}px`);
+            this.#root.style.setProperty("--pos-y", `${pos[1]}px`);
+        }
+
+        fontSize(size) {
+            if (typeof(size) !== "string" || size === "") return console.error("Set Size - Invalid Dimensions");
+    
+            this.#root.style.setProperty("--font-size", size);
+        }
+
+        corners(deg) {
+            if (deg <= 0) return console.error("Set Corners - Invalid Deg");
+
+            this.#root.style.setProperty("--corners", deg);
+        }
+
+        title(name) {
+            if (typeof(name) !== "string" || name === "") return console.error("Set Title - Invalid Name");
+
+            document.querySelector(".terminal-title").textContent = name;
+        }
     }
 }
--- debug/tmp.js
+++ debug/tmp.js
@@ -1,4 +1,23 @@
 import { terminal } from "../_Terminal/terminal.js";
 
 terminal.init();
-terminal.fontSize("20px")
+terminal.window.title("My Test Terminal");
+
+terminal.print(" Try Inputs tests :");
+terminal.skipLine();
+
+terminal.input("Test input 1", value1 => {
+    terminal.print("> input 1 done");
+
+    terminal.input("Test input 2", value2 => {
+        terminal.print("> input 2 done");
+
+        terminal.input("Test input 3", value3 => {
+            terminal.print("> input 3 done");
+
+            terminal.skipLine(2);
+
+            terminal.print(`Value 1 = ${value1}`, `Value 2 = ${value2}`, `Value 3 = ${value3}`);
+        })
+    })
+})
